aa260c9c634edce27f611b29178795749a19f193
Vito Polenek <vitopolenek@MacBook-Pro---Vito.local>
2024-12-14 22:35:10 +0100

    test

--- aplikacija/vite.config.js
+++ aplikacija/vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: "dist", // Default build directory, ensure it matches your `firebase.json`
-    sourcemap: true, // Optional: Generates sourcemaps for debugging
+    outDir: "dist", // Ensure this matches your Firebase configuration
+    sourcemap: true,
   },
 });
